008ede8a5847c980529d3bb8960e93cde7033c15
kairosity <rachelobrien@Karina1.local>
2020-08-30 14:18:59 +0100

    game complete

--- app.js
+++ app.js
@@ -51,6 +51,60 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ]
 
+cardArray.sort(() => 0.5 - Math.random());
+
+const grid = document.querySelector('.grid');
+const resultDisplay = document.querySelector('#result');
+var cardsChosen = [];
+var cardsChosenId = [];
+var cardsWon = [];
+//Create board here
+
+function createBoard() {
+    for (let i=0; i< cardArray.length; i++){
+        var card = document.createElement('img');
+        card.setAttribute('src', 'images/blank.jpg');
+        card.setAttribute('data-id', i);
+        card.addEventListener('click', flipCard);
+        grid.appendChild(card);
+    }
+}
+
+//check for matches
+function checkForMatch(){
+    var cards = document.querySelectorAll('img');
+    const optionOneId = cardsChosenId[0];
+    const optionTwoId = cardsChosenId[1];
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match!')
+        cards[optionOneId].setAttribute('src', 'images/white-tile.jpg');
+        cards[optionTwoId].setAttribute('src', 'images/white-tile.jpg');
+        cardsWon.push(cardsChosen);
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/blank.jpg');
+        cards[optionTwoId].setAttribute('src', 'images/blank.jpg');
+        alert('Sorry no match, please try again.');
+    }
+    cardsChosen = [];
+    cardsChosenId = [];
+    resultDisplay.textContent = cardsWon.length;
+    if (cardsWon.length === cardArray.length/2) {
+        resultDisplay.textContent = "Congratulations! You've won!"
+    }
+}
+
+//flip your card
+function flipCard(){
+    var cardId = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenId.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500);
+    }
+}
+
+createBoard();
+
 })
 
-const grid = document.querySelector.('.grid');
